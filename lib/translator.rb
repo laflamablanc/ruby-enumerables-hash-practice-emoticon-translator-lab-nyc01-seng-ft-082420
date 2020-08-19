3ac454cb91430fbcf76deb25ab365f456adc8856
Sean LaFlam <sean.laflam@gmail.com>
2020-08-19 17:02:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,22 +2,19 @@
 require "yaml"
 
 
-def load_library(hash)
-  new_emojis = hash.each_with_object({}) do |(outer_key, emoji_array), final_hash|
-    emoji_array.each_with_index do |emoji, idx|
-        if !final_hash[outer_key]
-            final_hash[outer_key] = {}
-        end
-        
-        if idx % 2 == 0 
+def load_library
+  # code goes here
+  emoticons = YAML.load_file('lib/emoticons.yml')
+  emoticons.each_with_object do |(outer_key, emoji_hash), final_hash]
+    emoji_hash.each_with_index do |emoji, idx|
+      if idx % 2 == 0 
         final_hash[outer_key][:english] = emoji
-        else
+      else
         final_hash[outer_key][:japanese] = emoji
-        end
-    
+      end
     end
   end
-  puts new_emojis
+  final_hash
 end
 
 def get_japanese_emoticon
